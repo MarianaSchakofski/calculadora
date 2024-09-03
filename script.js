cce1e2320295097ebcfc64c20c83a8f4decdf122
MarianaSchakofski <159267225+MarianaSchakofski@users.noreply.github.com>
2024-09-03 10:46:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 function calcula(opera){
-    let num1 = parseFloat(document.grtElementById('zeroum').value);
-    let num2 =parseFloat
+    let num1 = parseFloat(document.getElementById('zeroum').value);
+    let num2 =parseFloat(document.getElementById('zerodois').value);
+    let resultado = 0;
+    if(isNaN(num1) || isNaN(num2)){
+        alert('acorde para a vida.');
+
+
+    }else{
+        if(opera == '+'){
+            resultado = num1 + num2;
+        }else if(opera == '-'){
+            resultado = num1 - num2;
+        }else if(opera == '*'){
+            resultado = num1 * num2;
+        }else if(opera == '/');{
+            resultado = num1 / num2;
+        }
+        document.getElementById('resultado').textContent = 'resultado: ' + resultado;
+    }
 }
